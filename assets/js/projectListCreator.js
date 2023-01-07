84b5d4f069b4559b23d1e4bdb53072846b4cacc5
DarthReca-Convertibile <darthreca@gmail.com>
2023-01-07 13:26:28 +0100

    :lipstick: Added blank target and changed aspect of toolbar

--- assets/js/projectListCreator.js
+++ assets/js/projectListCreator.js
@@ -1,4 +1,4 @@
-const ELEMENTLIST = (x) => `<div class="hover:bg-primary hover:text-white"><a class="block px-6 py-2 border-b-2 border-primary text-center" href=${x.link}>${x.name}</a></div>`
+const ELEMENTLIST = (x) => `<div class="hover:bg-primary hover:text-white"><a class="block px-6 py-2 border-b-2 border-primary text-center" href=${x.link} target="_blank">${x.name}</a></div>`
 
 function makeList(json) 
 {
